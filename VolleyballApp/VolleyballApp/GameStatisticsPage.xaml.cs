5aa825ea76c4b3d3ca5df098059f847ca3a10ee0
Vegardhh <vegardheitloholm@hotmail.com>
2018-02-12 12:27:20 +0100

    New dynamic layout for statistics page

--- VolleyballApp/VolleyballApp/GameStatisticsPage.xaml.cs
+++ VolleyballApp/VolleyballApp/GameStatisticsPage.xaml.cs
@@ -15,17 +15,99 @@ namespace VolleyballApp
 		public GameStatisticsPage ()
 		{
 			InitializeComponent ();
-		}
+
+            using (SQLite.SQLiteConnection conn = new SQLite.SQLiteConnection(App.DB_PATH))
+            {
+                conn.CreateTable<Player>();
+                var players = conn.Table<Player>().ToList();
+                
+            
+            BackgroundColor = Color.Black;
+
+            var headerLabelStyle = new Style(typeof(Label))
+            {
+                Setters =
+                {
+                    new Setter{Property = Label.BackgroundColorProperty, Value = Color.White},
+                    new Setter {Property = Label.TextColorProperty, Value = Color.Black},
+                    new Setter{Property = Label.FontSizeProperty, Value = 40}
+                }
+            };
+
+            var nameLabelStyle = new Style(typeof(Label))
+            {
+                Setters =
+                {
+                    new Setter{Property = Label.BackgroundColorProperty, Value = Color.White},
+                    new Setter {Property = Label.TextColorProperty, Value = Color.Black},
+                    new Setter{Property = Label.FontSizeProperty, Value = 30}
+                }
+            };
+
+                var entryStyle = new Style(typeof(Entry))
+                {
+                    Setters =
+                {
+                    new Setter{Property = Entry.BackgroundColorProperty, Value = Color.White},
+                    new Setter {Property = Entry.TextColorProperty, Value = Color.Black},
+                    new Setter{Property = Entry.FontSizeProperty, Value = 20}
+                }
+                };
+                var controlGrid = new Grid { RowSpacing = 1, ColumnSpacing = 1 };
+            controlGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+            controlGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+            controlGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+            controlGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+            controlGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+            controlGrid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+
+            controlGrid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+            controlGrid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+            controlGrid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+            controlGrid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+            controlGrid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+            controlGrid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+
+            controlGrid.Children.Add(new Label { Text = "Name", Style = headerLabelStyle }, 0, 0);
+            controlGrid.Children.Add(new Label { Text = "Serv", Style = headerLabelStyle }, 1, 0);
+            controlGrid.Children.Add(new Label { Text = "Reception", Style = headerLabelStyle }, 2, 0);
+            controlGrid.Children.Add(new Label { Text = "Attack", Style = headerLabelStyle }, 3, 0);
+            controlGrid.Children.Add(new Label { Text = "Block", Style = headerLabelStyle }, 4, 0);
+            controlGrid.Children.Add(new Label { Text = "Dig", Style = headerLabelStyle }, 5, 0);
+
+                int colNr = 0;
+                int rowNr = 1;
+                for (int i = 0; i<players.Count; i++)
+                {
+                    string pName = players[i].name;
+                    controlGrid.Children.Add(new Label { Text = pName, Style = nameLabelStyle }, colNr, rowNr);
+                    colNr++;
+                    while(colNr < 6)
+                    {
+                        controlGrid.Children.Add(new Entry { Style = entryStyle }, colNr, rowNr);
+                        colNr++;
+                    }
+                    colNr = 0;
+                    rowNr++;
+                }
+                
+                
+
+            Content = controlGrid;
+            }
+        }
         protected override void OnAppearing()
         {
             base.OnAppearing();
-
+            /*
             using (SQLite.SQLiteConnection conn = new SQLite.SQLiteConnection(App.DB_PATH))
             {
                 conn.CreateTable<Player>();
                 var players = conn.Table<Player>().ToList();
 
                 var gridController = new Grid();
+            }
+            */
         }
     }
 }
