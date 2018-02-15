ec30ad2657993e997589c400a554132504917d10
Sigbjørn <sigmyh93@gmail.com>
2018-02-15 15:05:12 +0100

    ArchivePage now successfully deletes and updates!

--- VolleyballApp/VolleyballApp/ArchivePage.xaml.cs
+++ VolleyballApp/VolleyballApp/ArchivePage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,15 +14,20 @@ namespace VolleyballApp
 	public partial class ArchivePage : ContentPage
 	{
         public db.Database database;
+        public ObservableCollection<db.Game> gamesObs;
 
 		public ArchivePage ()
 		{
 			InitializeComponent ();
-
             database = new db.Database();
-            var games = database.GetGames().ToList();
 
-            gamesListView.ItemsSource = games;
+            // Create observable collection and populate from DB
+            gamesObs = new ObservableCollection<db.Game>();
+            database.GetGames().ToList()
+                .ForEach(gamesObs.Add);
+
+            // Add observable collection as ItemSource for ListView
+            gamesListView.ItemsSource = gamesObs;
         }
 
         public void ViewGame(object sender, EventArgs e)
@@ -31,10 +37,21 @@ namespace VolleyballApp
             // Open ViewGameStats
         }
 
+        // TODO: add cancelable delete
         public void DeleteGame(object sender, EventArgs e)
         {
-            // Delete the game
-            DisplayAlert("Info", "Delete Game", "OK");
+            // Get sent item from XAML
+            var item = (Xamarin.Forms.Button)sender;
+            db.Game game = (db.Game)item.CommandParameter;
+            
+            // Remove from database and collection
+            database.DeleteGame(game);
+            gamesObs.Remove(game);
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
         }
     }
 }
