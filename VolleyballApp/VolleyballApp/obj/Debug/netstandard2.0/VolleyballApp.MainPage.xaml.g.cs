bccacba12857b263f3e72ba31bd633ca0264c686
Sigbjørn <sigmyh93@gmail.com>
2018-02-16 12:19:20 +0100

    Merge all these debug files

--- VolleyballApp/VolleyballApp/obj/Debug/netstandard2.0/VolleyballApp.MainPage.xaml.g.cs
+++ VolleyballApp/VolleyballApp/obj/Debug/netstandard2.0/VolleyballApp.MainPage.xaml.g.cs
@@ -11,8 +11,8 @@
 namespace VolleyballApp {
     
     
-    [global::Xamarin.Forms.Xaml.XamlFilePathAttribute("C:\\Users\\141459\\Documents\\VisualStudioProjects\\VolleyballApp\\VolleyballApp\\Volley" +
-        "ballApp\\MainPage.xaml")]
+    [global::Xamarin.Forms.Xaml.XamlFilePathAttribute("C:\\Users\\142552\\source\\repos\\VolleyballApp\\VolleyballApp\\VolleyballApp\\MainPage.x" +
+        "aml")]
     public partial class MainPage : global::Xamarin.Forms.ContentPage {
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "0.0.0.0")]
