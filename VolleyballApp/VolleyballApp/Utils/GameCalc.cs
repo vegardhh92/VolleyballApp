a1c66da9d3faa84737e44fe1210fa596404247de
myhres <sigmyh93@gmail.com>
2018-02-18 21:08:16 +0100

    Refactored GameCalc
    GameCalc methods now returns 0 if it receives an empty string or null.
    All methods are now static for more ease of use. AvgReception method
    returns a rounded number similar to the other methods.

--- VolleyballApp/VolleyballApp/Utils/GameCalc.cs
+++ VolleyballApp/VolleyballApp/Utils/GameCalc.cs
@@ -6,84 +6,106 @@ namespace VolleyballApp.Utils
 {
     public class GameCalc
     {
-        public double AttackEfficiency(string attacks)
+        public static double AttackEfficiency(string attacks)
         {
-            double k = 0;
-            double e = 0;
-            double total = attacks.Length;
-            System.Diagnostics.Debug.WriteLine("Length: " + total);
-            if(total == 0)
+            if (attacks == null || attacks == "")
             {
                 return 0;
             }
-            foreach(Char c in attacks)
+            else
             {
-                if (c == '+')
+                double k = 0;
+                double e = 0;
+                double total = attacks.Length;
+                System.Diagnostics.Debug.WriteLine("Length: " + total);
+                if(total == 0)
                 {
-                    k++;
-                }else if(c == '-')
-                {
-                    e++;
+                    return 0;
                 }
+                foreach(Char c in attacks)
+                {
+                    if (c == '+')
+                    {
+                        k++;
+                    }else if(c == '-')
+                    {
+                        e++;
+                    }
+                }
+                double attackEff = 0;
+                attackEff = (k - e) / total;
+                System.Diagnostics.Debug.WriteLine("k: " + k + " e: " + e + " attackEff: " + attackEff);
+                return Math.Round(attackEff,2);
             }
-            double attackEff = 0;
-            attackEff = (k - e) / total;
-            System.Diagnostics.Debug.WriteLine("k: " + k + " e: " + e + " attackEff: " + attackEff);
-            return Math.Round(attackEff,2);
         }
 
-        public double ServEfficiency(string servs)
+        public static double ServEfficiency(string servs)
         {
-            double ace = 0;
-            double error = 0;
-            double total = servs.Length;
-            if (total == 0)
+            if (servs == null || servs == "")
             {
                 return 0;
             }
-            System.Diagnostics.Debug.WriteLine("Length: " + total);
-            foreach (Char c in servs)
+            else
             {
-                if (c == '+')
+                double ace = 0;
+                double error = 0;
+                double total = servs.Length;
+                if (total == 0)
                 {
-                    ace++;
+                    return 0;
                 }
-                else if (c == '-')
+                System.Diagnostics.Debug.WriteLine("Length: " + total);
+                foreach (Char c in servs)
                 {
-                    error++;
+                    if (c == '+')
+                    {
+                        ace++;
+                    }
+                    else if (c == '-')
+                    {
+                        error++;
+                    }
                 }
+                double servEff = 0;
+                servEff = (ace - error) / total;
+                System.Diagnostics.Debug.WriteLine("k: " + ace + " e: " + error + " servEfficency: " + servEff);
+                return Math.Round(servEff, 2);
             }
-            double servEff = 0;
-            servEff = (ace - error) / total;
-            System.Diagnostics.Debug.WriteLine("k: " + ace + " e: " + error + " servEfficency: " + servEff);
-            return Math.Round(servEff, 2);
         }
 
-        public double avgReception(string receptions)
+        public static double AvgReception(string receptions)
         {
-
-            double total = receptions.Length;
-
-            if (total == 0)
+            if (receptions == null || receptions == "")
             {
                 return 0;
             }
-            double avg = 0;
-            foreach(Char c in receptions)
+            else
             {
-                if (c == '1')
+                double total = receptions.Length;
 
-                    avg += 1;
-                else if (c == '2')
-                    avg += 2;
-                else if (c == '3')
-                    avg += 3;
-                else if (c == '4')
-                    avg += 4;
-                else if (c == '5')
-                    avg += 5;
+                if (total == 0)
+                {
+                    return 0;
+                }
+                double avg = 0;
+                foreach(Char c in receptions)
+                {
+                    if (c == '1')
+
+                        avg += 1;
+                    else if (c == '2')
+                        avg += 2;
+                    else if (c == '3')
+                        avg += 3;
+                    else if (c == '4')
+                        avg += 4;
+                    else if (c == '5')
+                        avg += 5;
+                }
+
+                avg = avg / total;
+                return Math.Round(avg, 2);
             }
-             return  avg = avg / total;
         }
     }
 }
